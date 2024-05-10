ed657d13f61034c1cf50a44cdd28469e97c51850
lei-mon <leilamontemayor37@gmail.com>
2024-05-10 16:59:51 +0800

    Renamed file for clarity

--- lib/pages/task/add_task.dart
+++ lib/pages/task/add_task.dart
@@ -2,14 +2,14 @@ import 'package:deck/pages/misc/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:deck/pages/misc/widget_method.dart';
 
-class TodoPage extends StatelessWidget {
-  const TodoPage({super.key});
+class AddTaskPage extends StatelessWidget {
+  const AddTaskPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: AuthBar(
-        title: 'sdsd',
+        title: 'Add New',
         color: DeckColors.white,
         fontSize: 24,
       ),
