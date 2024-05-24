a67c2fb4d46f58432b8737149e47617184f64d5c
Arthur Artugue <artuguearthur@gmail.com>
2024-05-24 16:35:08 +0800

    Finished add card using ai

--- lib/backend/flashcard/flashcard_ai_service.dart
+++ lib/backend/flashcard/flashcard_ai_service.dart
@@ -1,3 +1,117 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:deck/backend/models/cardAi.dart';
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
+import '../../firebase_options.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart' as path;
+
 class FlashcardAiService{
 
+  // Method to send data to api gateway
+  Future<Map<String, dynamic>> sendData({
+    required String id,
+    required String subject,
+    required String topic,
+    required String addDescription,
+    required String pdfFileName,
+    required int numberOfQuestions,
+    bool isNewMessage = true,
+    String threadID = "no_thread_id",
+  }) async {
+    // Define request body
+    Map<String, dynamic> requestBody = {
+      'subject': subject,
+      'topic': topic,
+      'numberOfQuestions': numberOfQuestions,
+      'pdfFileName': pdfFileName,
+      'addDescription': addDescription,
+      'isNewMessage': isNewMessage,
+      'threadID': threadID,
+    };
+
+    // Make the API call
+    final response = await http.post(
+      Uri.parse('http://10.0.2.2:8080/message/$id'), //API endpoint
+      body: jsonEncode(requestBody),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    // Check if the response is successful
+    if (response.statusCode == 200) {
+      // Parse the JSON data
+      final jsonData = jsonDecode(response.body);
+      return jsonData;
+    } else {
+      // If the server did not return a 200 OK response, throw an exception
+      print(response.statusCode);
+      throw Exception('Failed to send data: ${response.statusCode}');
+    }
+  }
+  //===========================================================================================
+
+  Future<List<Cardai>> fetchData({
+    required String id,
+    required String threadID,
+    required String runID,
+  }) async {
+
+    // Make the API call
+    final response = await http.get(
+      Uri.parse('http://10.0.2.2:8080/response/$id?thread_id=$threadID&run_id=$runID'), //API endpoint
+    );
+
+    // Check if the response is successful
+    if (response.statusCode == 200) {
+      // Parse the JSON data
+
+      var jsonData = jsonDecode(response.body) as List;
+      //var jsonDataQues = jsonDecode(jsonData)["question"] as List;
+      print(jsonData);
+
+      List<dynamic> questionsList = jsonData[0]['questions'];
+      List<Cardai> flashCards = [];
+      for (var questionAnswerPair in questionsList) {
+        String question = questionAnswerPair['question'];
+        String answer = questionAnswerPair['answer'];
+        Cardai flashcard = new Cardai(question: question, answer: answer);
+        flashCards.add(flashcard);
+        // Use question and answer as needed
+      }
+      //List<Flashcard> flashCards = jsonData.map((flashObj) => Flashcard.fromJson(flashObj)).toList();
+
+      return flashCards;
+    } else {
+
+      print(response.statusCode);
+
+      // If the server did not return a 200 OK response, throw an exception
+      throw Exception('Failed to fetch data: ${response.statusCode}');
+    }
+  }
+  Future<String> uploadFileToFirebase(String filePath) async {
+    String fileUrl = "";
+    File file = File(filePath);
+    String uniqueFileName = DateTime.now().millisecondsSinceEpoch.toString();
+    if(await file.exists()){
+      // Get a reference to the Firebase Storage location
+      Reference referenceRoot = FirebaseStorage.instance.ref();
+      Reference referenceFileToUpload = referenceRoot.child(uniqueFileName);
+
+      // Upload file to Firebase Storage
+      try {
+        await referenceFileToUpload.putFile(File(file.path));
+        print('File Uploaded Successfully!');
+      } catch (e) {
+        print('Error uploading file: $e');
+      }
+    }
+    return uniqueFileName;
+  }
 }
--- lib/pages/flashcard/add_deck.dart
+++ lib/pages/flashcard/add_deck.dart
@@ -1,11 +1,15 @@
+import 'package:deck/backend/flashcard/flashcard_ai_service.dart';
 import 'package:deck/backend/flashcard/flashcard_service.dart';
+import 'package:deck/backend/models/cardAi.dart';
 import 'package:deck/pages/flashcard/view_deck.dart';
 import 'package:deck/pages/misc/colors.dart';
 import 'package:deck/pages/misc/deck_icons.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:deck/pages/misc/widget_method.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:path/path.dart' as path;
 
 import '../../backend/models/deck.dart';
 
@@ -25,6 +29,11 @@ class AddDeckPage extends StatefulWidget {
 class _AddDeckPageState extends State<AddDeckPage> {
   bool _isToggled = false;
   final TextEditingController _deckTitleContoller = TextEditingController();
+  final TextEditingController _pickedFileController = TextEditingController();
+  final TextEditingController _subjectController = TextEditingController();
+  final TextEditingController _topicController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _numCardsController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -141,20 +150,20 @@ class _AddDeckPageState extends State<AddDeckPage> {
                   )),
               if (_isToggled) CustomExpansionTile(),
               if (_isToggled)
-                const Padding(
+                Padding(
                   padding: EdgeInsets.only(top: 20),
-                  child: BuildTextBox(hintText: 'Enter Subject'),
+                  child: BuildTextBox(hintText: 'Enter Subject', controller: _subjectController,),
                 ),
               if (_isToggled)
-                const Padding(
+                Padding(
                   padding: EdgeInsets.only(top: 20),
-                  child: BuildTextBox(hintText: 'Enter Topic'),
+                  child: BuildTextBox(hintText: 'Enter Topic', controller: _topicController,),
                 ),
               if (_isToggled)
-                const Padding(
+                Padding(
                   padding: EdgeInsets.symmetric(vertical: 20),
                   child: BuildTextBox(
-                      hintText: 'Enter Description', isMultiLine: true),
+                      hintText: 'Enter Description', isMultiLine: true, controller: _descriptionController,),
                 ),
               if (_isToggled)
                 Row(
@@ -162,7 +171,46 @@ class _AddDeckPageState extends State<AddDeckPage> {
                     Padding(
                       padding: const EdgeInsets.only(right: 10.0),
                       child: BuildButton(
-                        onPressed: () {},
+                        onPressed: () async{
+                          try {
+                            FilePickerResult? result = await FilePicker.platform.pickFiles(
+                              type: FileType.custom,
+                              allowedExtensions: ['pdf'],
+                            );
+
+                            if (result != null) {
+                              PlatformFile file = result.files.first;
+                              _pickedFileController.text = file.path ?? '';
+                            } else {
+                              // User canceled the picker
+                            }
+                          } catch (e) {
+                            print('Error: $e');
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('File Selection Error'),
+                                  content: const Text('There was an error in selecting the file. Please try again.'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(); // Close the dialog
+                                      },
+                                      child: const Text(
+                                        'Close',
+                                        style: TextStyle(
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+
+                        },
                         buttonText: 'Upload PDF',
                         height: 50,
                         width: 150,
@@ -174,16 +222,16 @@ class _AddDeckPageState extends State<AddDeckPage> {
                         textColor: DeckColors.gray,
                       ),
                     ),
-                    const Expanded(
+                    Expanded(
                       flex: 1,
-                      child: BuildTextBox(hintText: 'File Name'),
+                      child: BuildTextBox(hintText: 'File Name', controller: _pickedFileController,),
                     ),
                   ],
                 ),
               if (_isToggled)
-                const Padding(
+                Padding(
                   padding: EdgeInsets.only(top: 20),
-                  child: BuildTextBox(hintText: 'Enter Number Of FlashCards'),
+                  child: BuildTextBox(hintText: 'Enter Number Of FlashCards', controller: _numCardsController),
                 ),
               Padding(
                 padding: EdgeInsets.only(top: 35),
@@ -194,8 +242,148 @@ class _AddDeckPageState extends State<AddDeckPage> {
                     () async{
                       try{
                         if(_isToggled){
+                          //  START OF AI
+                          if(_deckTitleContoller.text.isNotEmpty && _numCardsController.text.isNotEmpty){
+                            try {
+                              FlashcardAiService _flashcardAiService = FlashcardAiService();
+                              String fileName = "";
 
+                              //Check if conditions are met before uploading file
+                              if (_pickedFileController.text.toString().trim().isNotEmpty &&
+                                  _numCardsController.text.toString().trim().isNotEmpty) {
+                                // Check if numberOfCards is a valid number and within the specified range
+                                int? numberOfCards = int.tryParse(_numCardsController.text);
+                                if (numberOfCards != null && numberOfCards > 0 && numberOfCards < 20) {
+                                  fileName = await _flashcardAiService.uploadFileToFirebase(_pickedFileController.text.toString().trim());
+                                }
+                              }
+
+                              //sendData function
+                              Map<String, dynamic> runAndThreadId = await _flashcardAiService.sendData(
+                                id: widget.userId,
+                                subject: _subjectController.text.trim(),
+                                topic: _topicController.text.trim(),
+                                addDescription: _descriptionController.text.trim(),
+                                pdfFileName: fileName,
+                                numberOfQuestions: int.tryParse(_numCardsController.text) ?? 0,
+                              );
+                              print(runAndThreadId);
+
+                              List<Cardai> flashCardDataList = await _flashcardAiService.fetchData(
+                                id: widget.userId,
+                                runID: runAndThreadId['run_id'],//"run_FKVsyJHmM7xCCJOHasmqORvR",
+                                threadID: runAndThreadId['thread_id'],//"thread_yqFz4pwS35nITyQ4dIG0xRc2"
+                              );
+                              print(flashCardDataList);
+
+                              if (flashCardDataList.isEmpty) {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: const Text('AI Did Not Give A Response'),
+                                      content: const Text('This usually happens if\n'
+                                          '1.) The subject, topic, or description given is inappropriate\n'
+                                          '2.) The request is not related to academics\n'
+                                          '3.) The uploaded file is a ppt converted to pdf\n'
+                                          '\nPlease check your input and try again'),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop(); // Close the dialog
+                                          },
+                                          child: const Text(
+                                            'Close',
+                                            style: TextStyle(
+                                              color: Colors.red,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              } else {
+                                // If sendData is successful, navigate to ViewDeckPage
+                                if(_deckTitleContoller.text.isNotEmpty){
+                                  FlashcardService _flashCardService = FlashcardService();
+                                  Deck? newDeck = await _flashCardService.addDeck(widget.userId, _deckTitleContoller.text.toString());
+                                  if(newDeck != null){
+                                    //Loop through the list and transfer info from response to the deck
+                                    for(Cardai aiResponse in flashCardDataList){
+                                      newDeck.addQuestionToDeck(
+                                          aiResponse.question.toString(),
+                                          aiResponse.answer.toString()
+                                      );
+                                    }
+
+                                    Navigator.pop(context, newDeck);
+
+                                    widget.decks.add(newDeck);
+
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => ViewDeckPage(deck: newDeck)),
+                                    );
+                                  }
+                                }else{
+                                  await Future.delayed(Duration(milliseconds: 300)); // Ensure the dialog is fully closed
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text('Input Error'),
+                                        content: const Text('Please fill out all of the input fields.'),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop(); // Close the dialog
+                                            },
+                                            child: const Text(
+                                              'Close',
+                                              style: TextStyle(
+                                                color: Colors.red,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                }
+                              }
+                            } catch (e) {
+                              // Handle any errors that occur during sendData
+                              print('Error: $e');
+                              // Optionally, you can show a snackbar or dialog to inform the user about the error
+                            }
+                          }else{
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('Input Error'),
+                                  content: const Text('Please fill out the fields for Deck title and number of cards'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(); // Close the dialog
+                                      },
+                                      child: const Text(
+                                        'Close',
+                                        style: TextStyle(
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+                          // END OF AI
                         }else{
+                          // START OF NON AI
                           if(_deckTitleContoller.text.isNotEmpty){
                             FlashcardService _flashCardService = FlashcardService();
                             Deck? newDeck = await _flashCardService.addDeck(widget.userId, _deckTitleContoller.text.toString());
@@ -208,7 +396,6 @@ class _AddDeckPageState extends State<AddDeckPage> {
                                 context,
                                 MaterialPageRoute(builder: (context) => ViewDeckPage(deck: newDeck)),
                               );
-
                             }
                           }else{
                             await Future.delayed(Duration(milliseconds: 300)); // Ensure the dialog is fully closed
